2fccc10202245f96db0f67a06813bf88503527c4
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-10-20 21:34:31 -0400

    Added javascript effects and added all the html pages

---
+++ nav.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $("#nav_home").click(function() {
+        window.location = "index.html";
+    });
+    $("#nav_cars").click(function() {
+        window.location = "cars.html";
+    });
+    $("#nav_lab").click(function() {
+        window.location = "lab.html";
+    });
+    $("#nav_team").click(function() {
+        window.location = "team.html";
+    });
+    $("#nav_drive").click(function() {
+        window.location = "drive.html";
+    });
+    $("#nav_games").click(function() {
+        window.location = "games.html";
+    });
+    $("#nav_contact").click(function() {
+        window.location = "contact.html";
+    });
+});

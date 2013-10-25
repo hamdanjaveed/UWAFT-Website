9276261eedabf931a6c1dfaebbf471917690062f
andyzhang1994 <zhangandy94@gmail.com>
2013-10-24 23:18:54 -0400

    Fixed minor bug on text

--- script/index.js
+++ script/index.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
     // animate the landing text to fade in and move down slightly
-    $("#landing_div_text").animate({top: "43%", opacity: 1.0}, 1000, function() {
+    $("#landing_image_text").animate({top: "43%", opacity: 1.0}, 1000, function() {
         // when the text is done animating, animate certain words to a green color
-        $("#landing_div_text span").animate({color:"#5EBA40"}, 1000);
+        $("#landing_image_text span").animate({color:"#5EBA40"}, 1000);
     });
     
     // evoked whenever the user scrolls
     $(window).scroll(function() {
         var currentWindowScrollTop = $(this).scrollTop();
         var newImageScrollTop = currentWindowScrollTop / 30.0;
-        $("#landing_div_text").css({top:43 - newImageScrollTop + "%"});
+        $("#landing_image_text").css({top:43 - newImageScrollTop + "%"});
     });
 });

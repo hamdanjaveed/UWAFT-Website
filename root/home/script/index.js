439ff996699fbec8d6197965c5fd7362ced29646
andyzg <zhangandy94@gmail.com>
2013-11-26 21:26:19 -0500

    Updated the template of the landing page

--- root/home/script/index.js
+++ root/home/script/index.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     // animate the landing text to fade in and move down slightly
     $("#landing_image_text").animate({top: "38%", opacity: 1.0}, 1000, function() {
         // when the text is done animating, animate certain words to a green color
-        $("#landing_image_text span").animate({color:"#5EBA40"}, 1000, function() {
+        $("#landing_image_text span").animate({color:"#5EBA40"}, 0, function() {
             setTimeout(function() {
                 $("#landing_image_text").animate({opacity:0.0}, 1000);
                 $("#landing_image_sharp").animate({opacity:1.0}, 1000, function() {

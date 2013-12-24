5f9e53d2ac3b32ed743a5dc123f396e95046baa4
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-12-24 15:08:27 -0500

    home.scss, did landing div and sponsorbar

--- root/page_home/script/index.js
+++ root/page_home/script/index.js
@@ -2,12 +2,12 @@ var time = 1000;
 
 $(document).ready(function() {
     // animate the landing text to fade in and move down slightly
-    $("#landing_image_text").animate({top: "44%", opacity: 1.0}, time, function() {
+    $("#uwaft_slogan").animate({top: "44%", opacity: 1.0}, time, function() {
         // when the text is done animating, animate certain words to a green color
-        $("#landing_image_text span").animate({color:"#5EBA40"}, time, function() {
+        $("#uwaft_slogan span").animate({color:"#5EBA40"}, time, function() {
             setTimeout(function() {
-                $("#landing_image_text").animate({opacity:0.0}, time);
-                $("#landing_div_blur").animate({opacity:0.0}, time, function() {
+                $("#uwaft_slogan").animate({opacity:0.0}, time);
+                $("#blurred_image").animate({opacity:0.0}, time, function() {
                     setTimeout(function() {
                         $(".car_info").animate({opacity:1.0}, time);
                     }, 500)
